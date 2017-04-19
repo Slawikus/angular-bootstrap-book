eac7afafdd46f416ee7ab4bcd06ca35002643e92
Slawa Gladkov <slawa.gladkov@invision.de>
2017-04-19 18:35:06 +0200

    Import ReactiveForms

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
 // import { RouterModule } from '@angular/router';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { SignInComponent } from './sign-in.component';
 
 @NgModule({
-    // imports: [ CommonModule, RouterModule, FormsModule, ReactiveFormsModule ],
+    imports: [ FormsModule, ReactiveFormsModule ],
     declarations: [ SignInComponent ],
     exports: [ SignInComponent ],
     providers: [ AuthService, AuthGuard ]
